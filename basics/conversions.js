fe6fcde92e1fc5c6c0f024897531203105217f55
Amit Biswas <amit.142biswas@gmail.com>
2023-10-15 17:53:39 +0530

    conversions

---
+++ basics/conversions.js
@@ -0,0 +1,15 @@
+let score = false
+let scores = 0
+
+// console.log(typeof score);
+// console.log(typeof (score));
+// let valInNum =Number(score)
+// let valInNum2 =Boolean(scores)
+// console.log(typeof valInNum);
+// console.log(valInNum);
+// console.log(valInNum2);
+// 24k5 = NaN (not a number)
+
+let Num = 33
+let dtr = String(Num)
+console.log(typeof dtr)
