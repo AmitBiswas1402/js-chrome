1836ebbf33ee790f88a9f492b36279f48c438031
Amit Biswas <amit.142biswas@gmail.com>
2023-10-15 16:51:11 +0530

    let const var initial js

--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,17 @@
 console.log("Amit")
+const accountId = 151564
+let accEmail = "amdbh@hmail.com"
+var accpassWord = 5465465
+accountCity = "Kolkata"
+let accountState;
+
+// accountId = 5562356
+// accEmail = "hgggfh"
+// passWord = 5644654
+// accountCity = "Melbourne"
+
+// console.log(accountId)
+// console.log(accEmail)
+// console.log(accpassWord)
+// console.log(accountCity)
+console.table([accountId, accEmail, accpassWord, accountCity, accountState])
