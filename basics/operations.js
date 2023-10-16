6b44888317828aa11bc15b926c2501442115de41
Amit Biswas <amit.142biswas@gmail.com>
2023-10-16 17:00:53 +0530

    operations

---
+++ basics/operations.js
@@ -0,0 +1,30 @@
+let value = 3
+let negValue = -value
+// console.log(negValue) 
+
+let a = "Amit"
+let b = " Biswas"
+// console.log(a+b)
+
+// console.log("2"+3)
+// console.log("2"-3)
+// console.log(2+"3"+9)  // If string is first then no get together not added
+// console.log(3+"3"+3+"6") 
+// console.log(3+"3"+3-"6") 
+// console.log(3+5+6+2+"8"+"6"+"4")
+// console.log(3+5+6+2+"8"-"6"+"4")
+
+// console.log(+true)
+// console.log(+"")
+
+let num1, num2, num3 = 6+4
+// console.log(num1) //only num3 will get defined
+// console.log(num2) //same as above
+// console.log(num3)
+
+let a1 = 55
+let a2 = 55
+a1++
+console.log(a1)
+++a2
+console.log(a2)
