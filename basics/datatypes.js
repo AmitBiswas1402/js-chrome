1fa45327fe8de37912c298c660bcad6cf6f9e6fd
Amit Biswas <amit.142biswas@gmail.com>
2023-10-17 17:06:46 +0530

    object function

--- basics/datatypes.js
+++ basics/datatypes.js
@@ -3,4 +3,28 @@ let isLoggedin = false
 let state = null
 
 // number, bigint, string, boolean, null, undefined, symbol, object
-console.log(typeof null)
+// console.log(typeof null)
+
+const id = Symb('15454')
+const anoth = Symb('15454')
+
+console.log(id === anoth)
+
+const func = function(){
+    console.log("Hwlo")
+}
+
+// Return type of variables in JavaScript
+// 1) Primitive Datatypes
+//        Number => number
+//        String  => string
+//        Boolean  => boolean
+//        null  => object
+//        undefined  =>  undefined
+//        Symbol  =>  symbol
+//        BigInt  =>  bigint
+
+// 2) Non-primitive Datatypes
+//        Arrays  =>  object
+//        Function  =>  function
+//        Object  =>  object
