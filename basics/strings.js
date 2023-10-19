38cea0047665b0ed5dc5e318e106bc2366904e46
Amit Biswas <amit.142biswas@gmail.com>
2023-10-19 22:31:37 +0530

    differnt string operation

---
+++ basics/strings.js
@@ -0,0 +1,23 @@
+const name = "name"
+const repo = 50
+
+// console.log("Hi, my name is", name ,"and my repo count is", repo) // Put value inside strings
+
+const game = new String('Amit')
+// console.log(game[0])
+// console.log(game.__proto__)
+// console.log(game.length)
+// console.log(game.toUpperCase())
+// console.log(game.toLowerCase())
+// console.log(game.charAt(3))
+// console.log(game.indexOf('t'))
+
+// const newStr = game.substring(0, 2)
+// console.log(newStr)
+
+const str = game.slice(-3, 4)
+console.log(str)
+
+const jj = "   oujhhu   "
+console.log(jj);
+console.log(jj.trim()); // trim cuts blank spaces
