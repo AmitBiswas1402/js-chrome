e6f97ffdb8ee26b7a4561726c99201e35bbfa355
Amit Biswas <amit.142biswas@gmail.com>
2023-10-24 17:31:28 +0530

    math random and math floor

--- basics/num-math.js
+++ basics/num-math.js
@@ -1,5 +1,15 @@
 const num = 12122.6352
-console.log(num.toPrecision(4));
+// console.log(num.toPrecision(4));
 
-const fyy= 68546
+const fyy= 5568546
+// console.log(fyy.toLocaleString());
 
+console.log(Math.random())
+console.log((Math.random()*10)+1)
+console.log(Math.floor(Math.random()*10)+1);
+// console.log(Math.floor()); NaN
+
+const min = 10
+const max = 15
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
