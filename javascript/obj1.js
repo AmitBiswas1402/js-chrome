df9100026a33ab31f9a6fed6667b22226ee7ee02
Amit Biswas <amit.142biswas@gmail.com>
2023-10-26 16:56:20 +0530

    folder renamed to javascript

---
+++ javascript/obj1.js
@@ -0,0 +1,12 @@
+//object literal is variable={
+//
+//}
+
+const jsUser={
+    name:"ABC",
+    age:23,
+    loaction:"XYZ",
+    email:"abc@mail.com",
+    isLoggedIn: false,
+    lastLogInDays: ["Monday", "Saturday"]
+}
