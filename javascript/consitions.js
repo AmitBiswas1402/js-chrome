82df81250c0499a3425e761b85f5fb1caaa61f5e
Amit Biswas <amit.142biswas@gmail.com>
2023-11-02 19:23:05 +0530

    conditional statements

---
+++ javascript/consitions.js
@@ -0,0 +1,35 @@
+// const temperature=45
+
+// if (temperature==="45"){
+//     console.log("yeah");
+// } else {
+//     console.log("nope");
+// }
+
+// const balance=100 //Implicit scope
+// if(balance>99) console.log("Ya"); //Don't use in next line
+
+// const obj=546
+// if(obj<500){
+//     console.log("less");
+// } else if(obj>550){
+//     console.log("more than 550");
+// } else {
+//     console.log("exact");
+// }
+
+const month = 2    //switch case
+switch (month) {
+    case 1:
+        console.log("Jan");
+        
+        break;
+    case 2:
+        console.log("Feb");
+            
+        break;
+
+    default:
+        console.log("Incorrect Input");
+        break;
+}
