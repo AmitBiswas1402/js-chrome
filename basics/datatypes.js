cebffc781179bef6f32274084fde5999631434a1
Amit Biswas <115923314+AmitBiswas1402@users.noreply.github.com>
2023-11-08 16:46:01 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,7 @@
+// datatypes = let const only (no var)
+let age = 18
+let isLoggedin = false
+let state = null
+
+// number, bigint, string, boolean, null, undefined, symbol, object
+console.log(typeof null)
