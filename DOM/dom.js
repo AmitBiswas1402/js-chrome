9bdc3c0e9bda55d7a74a0e29839784fce8904768
Amit Biswas <115923314+AmitBiswas1402@users.noreply.github.com>
2023-11-12 16:55:25 +0000

    DOM parent and children

---
+++ DOM/dom.js
@@ -0,0 +1,11 @@
+const parent = document.querySelector('.parent')
+// console.log(parent);
+// console.log(parent[1].innerHTML);
+
+for (var i = 0; i <parents.length; i++) {
+    console.log(parent.children[i].innerHTMl);
+}
+parent.children[1].style.color="red"
+// console.log(parent.firstElementChild);
+// console.log(parent.lastElementChild);
+
