5aed45095a6a1a348f07d25b6f3cdf323096eb0e
Amit Biswas <115923314+AmitBiswas1402@users.noreply.github.com>
2023-11-14 12:21:21 +0000

    add edit remove element

--- DOM/dom.js
+++ DOM/dom.js
@@ -1,12 +1,30 @@
-const div = document.createElement('div')
-console.log(div);
-div.className="main"
-div.id=Math.random(Math.random() * 10 +1)
-div.setAttribute("title", "generated title")
-div.style.backgroundColor="green"
-div.style.padding="12px"
-// div.innerText="chai aur code"
-const addText = document.createTextNode("Chai aur code")
-div.appendChild(addText)
+function addNewLanguage(langName){
+    const li=document.createElement('li');
+    li.innerHTML=`${langName}`
+    document.querySelector('.language').appendChild(li)
+}
+addNewLanguage("HTML")
+addNewLanguage("CSS")
 
-document.body.appendChild(div)
+function addAnotherLanguage(langname){  //The better way to run code
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode(langname))
+    document.querySelector('.language').appendChild(li)
+}
+addAnotherLanguage("JavaScript")
+// addAnotherLanguage("TypeScript")
+
+//Edit the values
+const editLanguage = document.querySelector("li:nth-child(0)")
+console.log(editLanguage);
+const NewLi=document.createElement('li')
+NewLi.textContext="Java"
+editLanguage.replaceWith(NewLi)
+
+//Another way to edit values
+const reditedLang=document.querySelector("li:nth-child(1)")
+reditedLang.outerHTML='<li>HTML</li>'
+
+//Removing element
+const removeElement = document.querySelector("li:last-child")
+removeElement.remove()
