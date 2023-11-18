17eaae5d1432e7fc821cc9b8d8cf676aaff8309d
Amit Biswas <115923314+AmitBiswas1402@users.noreply.github.com>
2023-11-18 16:54:06 +0000

    will complete tomorrow

---
+++ projects/Number game/script.js
@@ -0,0 +1,78 @@
+const randomNumber = parseInt(Math.random()*100+1);
+
+const submit = document.querySelector('#subt')
+const userInput = document.querySelector('#guessField')
+const guessSlot = document.querySelector('.guesses')
+const remaining = document.querySelector('.lastResult')
+const lowOrHi = document.querySelector('.lowOrHi')
+const startOver = document.querySelector('.resultParas')
+
+const p = document.createElement('p')
+
+let playGame = []
+let numGuess = 1
+let playGame = true
+
+if(playGame){
+  submit.addEventListener('click', function(e){
+    e.preventDefault()
+    const guess = parseInt(userInput.value)
+    console.log(guess)
+    validateGuess(guess)
+  })
+}
+
+function validateGuess(guess){
+  if(isNaN(guess)){
+    alert('Please enter a valid number')
+  } else if(guess < 1){
+    alert('Please enter a no more than 1')
+  } else if(guess > 100){
+    alert('Please enter a no less than 100')    
+  } else{
+    prevGuess.push(guess)
+    if(numGuess === 11){
+      displayGuess(guess)
+      displayMessage(`Game Over. Random number was ${randomNumber}`)
+      endGame()
+    } else {
+      displayGuess(guess)
+      checkGuess(guess)
+    }
+  }
+}
+
+function checkGuess(guess){
+  if(guess === randomNumber){
+    displayGuess(`You guessed it right`)
+  } else if(guess < randomNumber){
+    displayGuess(`Number is too low`)
+  } else if(guess > randomNumber){
+    displayGuess(`Number is too high`)
+  } 
+}
+
+function displayGuess(guess){
+  userInput.value = ''
+  guessSlot.innerHTML += `${guess}, `
+  numGuess++
+  remaining.innerHTML = `${11-numGuess}`
+}
+
+function displayMessage(message){
+  lowOrHi.innerHTML=`<h2>${message}</h2>`
+}
+
+function endGame(){
+  userInput.value=``
+  userInput.setAttribute('disables', '')
+  p.classList.add('button')
+  p.innerHTML=`<h2 id="newGame">Start new Game</h2>`
+  startOver.appendChild(p)
+  playGame = false
+  newGame();
+}
+
+function newGame(){
+
+}
