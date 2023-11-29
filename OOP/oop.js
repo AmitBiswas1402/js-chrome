d5ca9ca533a0179c3d35bf50f1e726265a9c2698
Amit Biswas <115923314+AmitBiswas1402@users.noreply.github.com>
2023-11-29 12:29:57 +0000

    oop constructors

--- OOP/oop.js
+++ OOP/oop.js
@@ -11,3 +11,21 @@ const user={
 
 console.log(user.username);
 console.log(user.getUserDetails());
+
+function User(username, loginCount, signedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.signedIn = signedIn;
+
+    this.greeting = function(){
+        console.log(`welcome ${this.username}`);
+    }
+
+    return this;
+}
+
+const userOne = new user("Amit", 12, true)
+const userTwo = new user("ABC", 11, false)
+
+console.log(userOne);
+// console.log(userTwo);
