96ea1749cfa6cd44384283b82934617a882c29da
Amit Biswas <115923314+AmitBiswas1402@users.noreply.github.com>
2023-11-30 13:32:04 +0000

    prototype

---
+++ OOP/object.js
@@ -0,0 +1,23 @@
+// function multipleBuy10(num){
+//     return num*5
+// }
+
+// multipleBuy10.power=2
+
+// console.log(multipleBuy10(5));
+// console.log(multipleBuy10.power);
+// console.log(multipleBuy10.prototype);
+
+function createUser(username, score){
+    this.username=username;
+    this.score=score;
+}
+
+createUser.prototype.increment = function(){
+    this.score++
+}
+
+const chai=createUser("chai", 25)
+const tea=createUser("tea", 250)
+
+myArray.prototype.map()
